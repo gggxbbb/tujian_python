bc9a3209afff3c38ad2b3adca56759b4efbf5ce7
gggxbbb <233190629@qq.com>
2019-07-02 19:35:27 +0800

    第xxx个版本

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# tujian_python
-一个使用 Python 编写的 Tujian 的简易工具
+# Tujian Python
+一个使用 Python 编写的 Tujian 的简易工具  
+  
+请使用 `Python3` 运行此工具
+
+# 快速开始
+前往 [Releases](https://github.com/gggxbbb/tujian_python/releases) 下载对应系统的最新版本
+
+# 如何使用
+## Windows
+确保拥有 `Git` 和 `Python3` 环境
+```
+git clone https://github.com/gggxbbb/tujian_python.git
+cd tujian_python
+python ./TujianP.py
+```
+
+## Linux
+确保拥有 `Git` 和 `Python3` 环境
+```
+git clone https://github.com/gggxbbb/tujian_python.git
+cd tujian_python
+python3 ./TujianP.py
+```
+
+## Mac OS
+没有 Mac
+
+## Android
+使用 `Termux`
+```
+apt install git python -y
+git clone https://github.com/gggxbbb/tujian_python.git
+cd tujian_python
+python3 ./TujianP.py
+```
---
+++ TujianP.py
@@ -0,0 +1,154 @@
+#coding=utf-8
+
+from urllib import request
+from urllib.request import urlretrieve
+from urllib import error
+import json
+import os
+
+#注意，游标卡尺的单位长度为4
+#请使用Python3
+
+header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+
+dir = './Tujian/'
+if not os.path.isdir(dir):
+    os.makedirs(dir)
+
+def getImage(name,pic):
+    title = pic['p_title'].replace('/','&').replace('\\','&')
+    date = pic['p_date']
+    pid = pic['PID']
+    link = pic['local_url']
+    user = pic['username']
+    path = './Tujian/%s-%s_%s_%s.%s.jpeg'%(date,name,title,pid,user)
+    if not os.path.isfile(path):
+        print('正在获取%s %s %s'%(name,title,link))
+        if (link.find('i.loli.net') != -1) or (link.find('ooo.0o0.ooo') != -1):
+            print('站外图片，跳过')
+        else:
+            req = request.Request(link, headers=header)
+            data = request.urlopen(req).read()
+            with open(path, 'wb') as f:
+                f.write(data)
+                f.close()
+                print('%s 已保存'%path)
+    else:
+        print("%s 已存在"%path)
+
+def getPics(name,tid,page):
+    print('获取 %s 第 %s 页'%(name,page))
+    reqPicsUrl = request.Request(url='https://api.dpic.dev/list/?page=%s&size=15&sort=%s'%(page,tid),
+    headers=header)
+    reqPics = request.urlopen(reqPicsUrl)
+    dataPics = json.loads(reqPics.read().decode('utf-8'))
+    print('获取 %s 第 %s 页原始数据:OK'%(name,page))
+    for pic in dataPics["result"]:
+        getImage(name,pic)
+    print('获取 %s 第 %s 页:OK'%(name,page))
+    if (page < dataPics['maxpage']):
+        print('%s 共%s页'%(name,dataPics['maxpage']))
+        getPics(name,tid,page +1)
+    else:
+        print('获取 %s :OK'%name)
+
+def getSort():
+    print('获取分类')
+    reqSortUrl=request.Request(url='https://api.dpic.dev/sort/',
+    headers=header)
+    reqSort = request.urlopen(reqSortUrl)
+    dataSort = reqSort.read().decode('utf-8')
+    tujianSort = json.loads(dataSort)
+    print('获取分类:OK')
+    return tujianSort["result"]
+
+def getAll():
+    picSort = getSort()
+    for m in picSort:
+        sortName = m["T_NAME"]
+        sortTID = m["TID"]
+        print('获取 %s TID: %s'%(sortName,sortTID))
+        getPics(sortName,sortTID,1)
+
+def chooseSort(sort):
+    picSort = {}
+    print('请选择分类')
+    for m in sort:
+        sortName = m["T_NAME"]
+        sortTID = m["TID"]
+        print('>%s'%sortName)
+        picSort[sortName] = sortTID
+    print('输入 0 退出')
+    print('输入 1 返回操作列表')
+    iSort = input('输入分类名>')
+    if iSort == "0":
+        exit()
+    elif iSort == "1":
+        return
+    else:
+        try:
+            getPics(iSort,picSort[iSort],1)
+        except:
+            print('错误 请重试')
+            chooseSort(sort)
+    
+
+def getPicBySort():
+    chooseSort(getSort())
+
+def getToday():
+    reqTodayUrl=request.Request(url='https://api.dpic.dev/today/',
+    headers=header)
+    reqToday = request.urlopen(reqTodayUrl)
+    dataToday = reqToday.read().decode('utf-8')
+    tujianToday = json.loads(dataToday)
+    print('获取今日图片原始数据:OK')
+    for pic in tujianToday:
+        getImage(pic['T_NAME'],pic)
+    print('获取今日图片:OK')
+        
+def serverTest():
+    try:
+        print('尝试与服务器建立连接')
+        reqTestUrl=request.Request(url='https://api.dpic.dev/',
+        headers=header)
+        request.urlopen(reqTestUrl)
+    except error.HTTPError as err:
+        print('服务器已去世 错误信息: %s'%err)
+    except:
+        print('发生错误 请重试')
+    else:
+        print('服务器正常')
+        print('>>>TujianP<<<')
+        start()
+
+def start():
+    print('选择操作:')
+    print('0.退出')
+    print('1.获取全部')
+    print('2.获取今日')
+    print('3.按分类获取全部')
+    print('98.搜索')
+    print('99.关于')
+    print('100.服务器测试')
+    event = input('请输入操作前数字>')
+    if event == "0":
+        exit()
+    elif event == '1':
+        getAll()
+    elif event == '2':
+        getToday()
+    elif event == '3':
+        getPicBySort()
+    elif event == '98':
+        print('请至 AppStore(Apple) 或其它应用商店(Android)下载 Tujian 或其它第三方版本体验此功能和其它高级功能')
+    elif event == "99":
+        print('这是一个使用 Python 编写的，用于获取 Tujian 图片的简易工具')
+    elif event == '100':
+        serverTest()
+    else:
+        print('输入错误 请重试')
+    start()
+        
+#start()
+serverTest()
