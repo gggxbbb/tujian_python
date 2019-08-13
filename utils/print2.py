13c0c583de77e5df55b7846ad0eaf186ea22ab43
gggxbbb <2331490629@qq.com>
2019-08-13 20:46:55 +0800

    bug fixed

--- utils/print2.py
+++ utils/print2.py
@@ -9,19 +9,19 @@ def ifWindows():
         return False
 
 def error(message):
-    if ifWindows:
+    if ifWindows():
         print(message)
     else:
         print('\033[31m%s\033[0m' % message)
 
 def success(message):
-    if ifWindows:
+    if ifWindows():
         print(message)
     else:
         print('\033[32m%s\033[0m' % message)
 
 def waring(message):
-    if ifWindows:
+    if ifWindows():
         print(message)
     else:
         print('\033[33m%s\033[0m' % message)
@@ -33,21 +33,21 @@ class print2:
         stdout.flush()
     @staticmethod
     def success(message):
-        if ifWindows:
+        if ifWindows():
             stdout.write(message)
         else:
             stdout.write('\033[32m%s\033[0m' % message)
         stdout.flush()
     @staticmethod
     def error(message):
-        if ifWindows:
+        if ifWindows():
             stdout.write(message)
         else:
             stdout.write('\033[31m%s\033[0m' % message)
         stdout.flush()
     @staticmethod
     def waring(message):
-        if ifWindows:
+        if ifWindows():
             stdout.write(message)
         else:
             stdout.write('\033[33m%s\033[0m' % message)
